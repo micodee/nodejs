3e69cbe43b6dc17e1cf6b6739af9f254985b5ee8
micodee <martomhidayat>
2022-10-29 14:37:44 +0700

    menambahkan table ke db

---
+++ tutorial-crud/controllers/UserController.js
@@ -0,0 +1,12 @@
+import User from "..//models/UserModel.js";
+
+export const getUsers = async(req, res) =>{
+    try{
+        const response = await User.findAll();
+        req.status(200).json(response);
+        res.status(200).json(response);
+    }
+    catch (error){
+        console.log(error.message);
+    }
+}
